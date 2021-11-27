3ba5c5b226a60169520e3fe4df066cad4c417f9a
mattias <mattias.decharleroy@gmail.com>
2021-11-27 15:58:31 +0100

    Use single argument key function in algos + add functional module

--- utils/algos.py
+++ utils/algos.py
@@ -1,6 +1,8 @@
 import operator
 
-def is_sorted(r, key=operator.le):
+from .functional import identity
+
+def is_sorted(r, key=identity):
 	r_shifted = iter(r)
 	next(r_shifted)
-	return all(key(a, b) for a, b in zip(r, r_shifted))
+	return all(key(a) <= key(b) for a, b in zip(r, r_shifted))
