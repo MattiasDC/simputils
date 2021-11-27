3e876d97f1321d89578fb5e60a51178eeb79716a
mattias <mattias.decharleroy@gmail.com>
2021-11-27 18:33:50 +0100

    Implement all_equal function

--- utils/algos.py
+++ utils/algos.py
@@ -2,9 +2,15 @@ import operator
 
 from .functional import identity
 
-def is_sorted(r, key=identity):
+def is_sorted(iterable, key=identity):
 	sentinel = object()
-	r_shifted = iter(r)
-	if next(r_shifted, sentinel) is sentinel:
+	iterable_shifted = iter(r)
+	if next(iterable_shifted, sentinel) is sentinel:
 		return True # empty iterable
-	return all(key(a) <= key(b) for a, b in zip(r, r_shifted))
+	return all(key(a) <= key(b) for a, b in zip(iterable, iterable_shifted))
+
+from itertools import groupby
+
+def all_equal(iterable, key=identity):
+    g = groupby(iterable, key)
+    return next(g, True) and not next(g, False)
