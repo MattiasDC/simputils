105c12ad342a31e46d1fd770904eaad4f7939b73
mattias <mattias.decharleroy@gmail.com>
2021-11-27 18:10:15 +0100

    Bugfix is_sorted, cope with empty iterables

--- utils/algos.py
+++ utils/algos.py
@@ -3,6 +3,8 @@ import operator
 from .functional import identity
 
 def is_sorted(r, key=identity):
+	sentinel = object()
 	r_shifted = iter(r)
-	next(r_shifted)
+	if next(r_shifted, sentinel) is sentinel:
+		return True # empty iterable
 	return all(key(a) <= key(b) for a, b in zip(r, r_shifted))
