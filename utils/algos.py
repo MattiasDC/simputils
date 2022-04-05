1b3a05cc9ba1afd3fbbdfd5b473a115c3a9d6882
mattias <mattias.decharleroy@gmail.com>
2022-04-05 15:15:44 +0200

    Add split_elements functionality

--- utils/algos.py
+++ utils/algos.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
 from itertools import groupby
+
+from more_itertools import flatten
 
 from .functional import identity
 
@@ -14,3 +17,17 @@ def is_sorted(iterable, key=identity):
 def all_equal(iterable, key=identity):
     g = groupby(iterable, key)
     return next(g, True) and not next(g, False)
+
+
+"""
+Return an iterable of lists.
+Each list contains elements which are considered equal according to the key.
+This algorithms can be used to split elements which are not sortable
+"""
+
+
+def split_elements(iterable, key=identity):
+    mappings = defaultdict(list)
+    for e in iterable:
+        mappings[key(e)].append(e)
+    return flatten(mappings.values())
