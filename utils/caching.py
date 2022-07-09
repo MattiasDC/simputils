e57c2a507fc4038b6a3a90e65e60153aa1ae17ed
mattias <mattias.decharleroy@gmail.com>
2022-07-09 12:52:12 +0200

    Add conditionally_cached decorator

---
+++ utils/caching.py
@@ -0,0 +1,26 @@
+from aiocache import cached
+
+
+class conditionally_cached(cached):
+    """
+    An async cache which conditionally caches values based on a function.
+    This implementation is built on top of
+    [cached](https://aiocache.readthedocs.io/en/latest/caches.html#cache)
+    """
+
+    def __init__(self, condition, **kwargs):
+        super().__init__(**kwargs)
+        self.condition = condition
+
+    async def decorator(self, f, *args, **kwargs):
+        key = self.get_cache_key(f, args, kwargs)
+
+        value = await self.get_from_cache(key)
+        if value is not None:
+            return value
+
+        result = await f(*args, **kwargs)
+        if self.condition(result):
+            await self.set_in_cache(key, result)
+
+        return result
